b47463345ad534663ad0802202fb1a09a7a9a473
Jukka Huhtamäki <jukka.huhtamaki@tut.fi>
2016-02-11 10:28:08 +0200

    Simple example on accessing Twitter REST API

---
+++ twitter-api/simple_read.py
@@ -0,0 +1,55 @@
+import simplejson as json
+
+with open('keychain.json') as f:
+  keychain = json.load(f)
+
+print keychain.keys()
+
+from requests_oauthlib import OAuth1
+
+def get_oauth():
+  oauth = OAuth1(keychain['CONSUMER_KEY'],
+              client_secret=keychain['CONSUMER_SECRET'],
+              resource_owner_key=keychain['OAUTH_TOKEN'],
+              resource_owner_secret=keychain['OAUTH_TOKEN_SECRET'])
+  return oauth
+
+oauth = get_oauth()
+
+import requests
+# https://twitter.com/osulop/status/697391618492604416
+
+# https://twitter.com/jnkka/status/696702852736090112
+
+url = 'https://api.twitter.com/1.1/statuses/show.json?id=696702852736090112'
+r = requests.get(url=url, auth=get_oauth())
+
+
+
+data = json.loads(r.json())
+
+print data
+
+with open('example.json','w') as f:
+  f.write(r.json())
+
+# print dir(r.json)
+
+# 'https://api.twitter.com/1.1/search/tweets.json?q=%40twitterapi', )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
