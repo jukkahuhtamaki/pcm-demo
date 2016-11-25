835991cd3beac50dcf8a5a106a12e9aa7f0399a2
Jukka Huhtamäki <jukka.huhtamaki@tut.fi>
2016-11-25 12:11:00 +0200

    Example of using Flask and Highcharts added

---
+++ flask/helloworld.py
@@ -0,0 +1,25 @@
+# from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "Hello World!"
+
+@app.route('/ok')
+def example():
+  return render_template('visualize.html', data=[])
+
+#  return 'Ok'
+
+@app.route('/dashboard')
+def visualize():
+#  print 'Dashboard'
+#  flash('New entry was successfully posted')
+  return 'Dashboard'
+  #  return render_template('visualize.html', data=data)
+
+if __name__ == "__main__":
+  app.run()
